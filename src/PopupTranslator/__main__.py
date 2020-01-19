fed5e950475a4ad0fce338f2b355c7df6e3e8ab4
Hassan Pouralijan <pouralijan@gmail.com>
2020-01-19 14:53:22 +0330

    Fix import bug.

--- src/PopupTranslator/__main__.py
+++ src/PopupTranslator/__main__.py
@@ -2,7 +2,7 @@
 import sys
 from PySide2.QtWidgets import QApplication
 
-from .PopupTranslator import PopupTranslatorWidget
+from PopupTranslator import PopupTranslatorWidget
 
 def main():
     app = QApplication(sys.argv)
